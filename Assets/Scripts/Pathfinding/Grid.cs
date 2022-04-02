d84224d908ca8402ea8681af308889337d1e4b8b
TriDivide <liamsmitha@gmail.com>
2022-04-02 23:32:14 +0100

    - fixed issue with permissions

--- Assets/Scripts/Pathfinding/Grid.cs
+++ Assets/Scripts/Pathfinding/Grid.cs
@@ -72,7 +72,7 @@ public class Grid<TGridObject> {
         }
     }
 
-    private void GetXY(Vector3 worldPosition, out int x, out int y) {
+    public void GetXY(Vector3 worldPosition, out int x, out int y) {
         x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
         y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
     }
