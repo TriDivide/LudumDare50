b36f9888e3c62d9b3592769e55845812df93b46a
TriDivide <liamsmitha@gmail.com>
2022-04-02 19:49:31 +0100

    - visualising grid cells.

--- Assets/Scripts/Pathfinding/Grid.cs
+++ Assets/Scripts/Pathfinding/Grid.cs
@@ -21,9 +21,14 @@ public class Grid {
         for(int x = 0; x < gridArray.GetLength(0); x++) {
             for (int y = 0; y < gridArray.GetLength(1); y++) {
                 Debug.Log(x + " , " + y);
-                GenerateGridSquareText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y), 20);
+                GenerateGridSquareText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize)  * .5f, 20);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
             }
         }
+
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
     }
 
     private Vector3 GetWorldPosition(int x, int y) {
