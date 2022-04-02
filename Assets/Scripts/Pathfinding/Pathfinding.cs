6c1301ba18c82298a5abaa48a2e654bc8633bdb5
TriDivide <liamsmitha@gmail.com>
2022-04-02 22:19:35 +0100

    - properly wrote the calculate path function - removed temporary code.

--- Assets/Scripts/Pathfinding/Pathfinding.cs
+++ Assets/Scripts/Pathfinding/Pathfinding.cs
@@ -104,7 +104,17 @@ public class Pathfinding {
     }
 
     private List<PathNode> CalculatePath(PathNode endNode) {
-        return null;
+        List<PathNode> path = new List<PathNode>();
+        path.Add(endNode);
+
+        PathNode currentNode = endNode;
+        while (currentNode.cameFromNode != null) {
+            path.Add(currentNode.cameFromNode);
+            currentNode = currentNode.cameFromNode;
+        }
+
+        path.Reverse();
+        return path;
     }
 
     private int CalculateDistanceCost(PathNode a, PathNode b) {
