e2d2613e230c07e9f343af61004de660fa0acb80
TriDivide <liamsmitha@gmail.com>
2022-04-02 11:03:52 +0100

    - zombie spawning working

---
+++ Assets/Scripts/ZombieSpawnerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawnerController : MonoBehaviour
+{
+
+    private GameObject[] spawners;
+    public GameObject zombie;
+
+    // Start is called before the first frame update
+    void Start() {
+        this.spawners = GameObject.FindGameObjectsWithTag("Spawner");
+
+        InvokeRepeating("SpawnZombie", 0f, 10f);
+
+    }
+
+
+    private void SpawnZombie() {
+        if(this.spawners != null) {
+            GameObject spawner = this.spawners[Random.Range(0, spawners.Length)];
+
+            Instantiate(zombie, spawner.transform.position, spawner.transform.rotation);
+        }
+    }
+
+
+
+}
