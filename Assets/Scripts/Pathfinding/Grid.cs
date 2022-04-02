89cb61c6c6902268445a5c91590972ac5ed24df4
TriDivide <liamsmitha@gmail.com>
2022-04-02 19:57:17 +0100

    - contined adding debug code.

--- Assets/Scripts/Pathfinding/Grid.cs
+++ Assets/Scripts/Pathfinding/Grid.cs
@@ -8,6 +8,8 @@ public class Grid {
     private int height;
     private float cellSize;
 
+    private TextMesh[,] debugTextArray;
+
     private int[,] gridArray;
     public Grid(int width, int height, float cellSize) {
 
@@ -16,12 +18,11 @@ public class Grid {
         this.cellSize = cellSize;
 
         gridArray = new int[width, height];
-        Debug.Log(width + " " + height);
+        debugTextArray = new TextMesh[width, height];
 
         for(int x = 0; x < gridArray.GetLength(0); x++) {
             for (int y = 0; y < gridArray.GetLength(1); y++) {
-                Debug.Log(x + " , " + y);
-                GenerateGridSquareText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize)  * .5f, 20);
+                debugTextArray[x, y] = GenerateGridSquareText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize)  * .5f, 20);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
             }
@@ -29,10 +30,19 @@ public class Grid {
 
         Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
         Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+
+        SetValue(2, 1, 56);
     }
 
     private Vector3 GetWorldPosition(int x, int y) {
         return new Vector3(x, y) * cellSize;
+    }
+
+    public void SetValue(int x, int y, int value) {
+        if (x >= 0 && y >= 0 && x < width && y < height) {
+            gridArray[x, y] = value;
+            debugTextArray[x, y].text = gridArray[x, y].ToString();
+        }
     }
 
     private TextMesh GenerateGridSquareText(string text, Transform parent = null, Vector3 localPosition = default(Vector3), int fontSize = 40) {
