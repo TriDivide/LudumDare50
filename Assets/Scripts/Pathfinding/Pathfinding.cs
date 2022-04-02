5a19834cb6291b7694cbb73c88ca0e853b77f3e6
TriDivide <liamsmitha@gmail.com>
2022-04-02 21:46:20 +0100

    - most code for finding the path has now been written. still need to finish the function for calculating the path

--- Assets/Scripts/Pathfinding/Pathfinding.cs
+++ Assets/Scripts/Pathfinding/Pathfinding.cs
@@ -4,10 +4,66 @@ using UnityEngine;
 
 public class Pathfinding {
 
+    private const int MOVE_STRAIGHT_COST = 10;
+    private const int MOVE_DIAGONAL_COST = 14;
 
     private Grid<PathNode> grid;
+    private List<PathNode> openList;
+    private List<PathNode> closedList;
 
     public Pathfinding(int width, int height) {
         grid = new Grid<PathNode>(width, height, 10f, Vector3.zero, (Grid<PathNode> g, int x, int y) => new PathNode(g, x, y));
+    }
+
+    private List<PathNode> FindPath(int startX, int startY, int endX, int endY) {
+        PathNode startNode = grid.GetGridObject(startX, startY);
+        PathNode endNode = grid.GetGridObject(endX, endY);
+        openList = new List<PathNode> { startNode };
+        closedList = new List<PathNode> { };
+
+        for (int x = 0; x < grid.GetWidth(); x++) {
+            for (int y = 0; y < grid.GetHeight(); y++) {
+                PathNode pathNode = grid.GetGridObject(x, y);
+                pathNode.gCost = int.MaxValue;
+                pathNode.CalculateFCost();
+                pathNode.cameFromNode = null;
+            }
+        }
+
+        startNode.gCost = 0;
+        startNode.hCost = CalculateDistanceCost(startNode, endNode);
+
+        startNode.CalculateFCost();
+
+
+        while (openList.Count > 0) {
+            PathNode currentNode = GetLowestFCostNode(openList);
+
+            if(currentNode == endNode) {
+                // We have reached the final node!
+                return CalculatePath(endNode);
+            }
+        }
+    }
+
+    private List<PathNode> CalculatePath(PathNode endNode) {
+        return null;
+    }
+
+    private int CalculateDistanceCost(PathNode a, PathNode b) {
+        int xDistance = Mathf.Abs(a.x - b.x);
+        int YDistance = Mathf.Abs(a.y - b.y);
+        int remaining = Mathf.Abs(xDistance - YDistance);
+        return MOVE_DIAGONAL_COST * Mathf.Min(xDistance, YDistance) + MOVE_STRAIGHT_COST * remaining;
+    }
+
+    private PathNode GetLowestFCostNode(List<PathNode> pathNodeList) {
+        PathNode lowestFCostNode = pathNodeList[0];
+        for (int i = 1; i < pathNodeList.Count; i++) {
+            if(pathNodeList[i].fCost < lowestFCostNode.fCost) {
+                lowestFCostNode = pathNodeList[0];
+            }
+        }
+        return lowestFCostNode;
     }
 }
