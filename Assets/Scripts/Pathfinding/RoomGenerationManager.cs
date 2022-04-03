7f38e02729f3a57c527b7564e3016face819b945
TriDivide <liamsmitha@gmail.com>
2022-04-03 09:08:18 +0100

    - renamed class to better represent what it is doing.

--- Assets/Scripts/Pathfinding/RoomGenerationManager.cs
+++ Assets/Scripts/Pathfinding/RoomGenerationManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Testing : MonoBehaviour {
+public class RoomGenerationManager : MonoBehaviour {
 
     private Pathfinding pathfinding;
 
