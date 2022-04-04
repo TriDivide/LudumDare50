3e6af505452107698284c432d8903d0f75cf932a
TriDivide <liamsmitha@gmail.com>
2022-04-04 20:22:19 +0100

    fixed null pointer

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Bullet : MonoBehaviour {
 
 
-    public AudioSource gunshot;
+    //public AudioSource gunshot;
 
     // Start is called before the first frame update
     void Start() {
 
-        gunshot.Play();
+       // gunshot.Play();
         Invoke("Despawn", 2f);
     }
 
