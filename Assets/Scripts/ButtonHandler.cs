cdeab3a5a2dc856ffe3631cdce60f061a2281cd7
TriDivide <liamsmitha@gmail.com>
2022-04-05 00:46:24 +0100

    - tweaked the start screen to better fit wide screens - navigating to high score works.

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -21,4 +21,8 @@ public class ButtonHandler : MonoBehaviour {
         SceneManager.LoadScene(sceneName: "StartScene");
     }
 
+    public void GoToHighscore() {
+        SceneManager.LoadScene(sceneName: "HighScoreScene");
+    }
+
 }
