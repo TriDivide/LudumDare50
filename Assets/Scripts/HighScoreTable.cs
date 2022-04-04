216425b2e2b7ff3fbe564b9fa0eb9877c3457680
TriDivide <liamsmitha@gmail.com>
2022-04-04 22:32:59 +0100

    - items are being drawn based off of the template.

--- Assets/Scripts/HighScoreTable.cs
+++ Assets/Scripts/HighScoreTable.cs
@@ -15,9 +15,13 @@ public class HighScoreTable : MonoBehaviour {
         entryTemplate.gameObject.SetActive(false);
 
 
+        float templateHeight = 20f;
         for (int i = 0; i < 10; i++) {
             Transform entryTransform = Instantiate(entryTemplate, entryContainer);
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+            entryTransform.gameObject.SetActive(true);
         }
     }
 }
